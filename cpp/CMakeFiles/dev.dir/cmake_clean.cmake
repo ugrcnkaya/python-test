fa074c4c143772858995a46449fe01b99b1ff404
root <11018661@stud.hocschule-heidelberg.de>
2023-02-15 12:10:28 +0000

    CPP Script

---
+++ cpp/CMakeFiles/dev.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dev.dir/main.cpp.o"
+  "CMakeFiles/dev.dir/main.cpp.o.d"
+  "dev"
+  "dev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
