a1b9056f249db7fa5cae427b371f72c3054797c2
root <11018661@stud.hocschule-heidelberg.de>
2023-02-15 17:51:34 +0000

    c++ basic class structure

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
+
+
+class Car {
+	public:
+	string name; 
+	string model;
+
+	virtual void see(){
+	cout << this->name << " " << this->model << " " << endl;
+	}
+
+void set(string a, string b){
+this->name = a;
+this->model = b;
+}
+
+};
+
+
 int main(){
-	cout << "Hello World!\n" << endl;
+	Car car1;
+	car1.set("bambam", "audi");
+	car1.see();
 
+	
 	return 0;
-
 }
